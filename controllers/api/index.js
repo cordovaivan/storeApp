903d9db55851cb52e7dc9348e85778d9f6625a4e
Ivan C <cordovaivan8@gmail.com>
2022-08-13 11:27:13 -0700

    set up index.js controller

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,8 @@
-const router = require
+const router = require('express').Router();
+const clothesRoutes = require('./clothesRoutes');
+const userRoutes = require('./userRoutes');
+
+router.use('./users', userRoutes);
+router.use('/clothes', clothesRoutes);
+
+module.exports = router;
