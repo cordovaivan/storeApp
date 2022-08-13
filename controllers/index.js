85b6a1ebc843c81b195867b9ba7d0b0d2b61e1f0
azariahgarrido <azariahdanielgarrido@gmail.com>
2022-08-13 12:00:28 -0700

    next draft

--- controllers/index.js
+++
@@ -1,9 +0,0 @@
-// const router = require('express').router();
-
-// const apiRoutes = require('./api');
-// const homeRoutes = require('./homeRoutes');
-
-// router.use('/', homeRoutes);
-// router.use('/api', apiRoutes);
-
-// module.exports = router;
