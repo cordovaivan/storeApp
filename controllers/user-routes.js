8ec2c88a65d76113b74fb7a3e0b2eb432acf8ba5
Ivan C <cordovaivan8@gmail.com>
2022-08-13 11:22:51 -0700

    added index.js file in controllers

--- controllers/user-routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const path = require('path');
-
-router.get('/', async (req, res) => {
-    res.sendFile(path.join(__dirname, '../views/layouts/main.handlebars'));
-});
-
-module.exports = router;
